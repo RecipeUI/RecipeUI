09955a7353a1225883a2ceb0ab294509c7e249da
Samuel San <samuelchsan@gmail.com>
2023-08-10 14:34:15 -0700

    modify colors for light and dark mode

--- ui/src/components/RecipeHome/RecipeHome.tsx
+++ ui/src/components/RecipeHome/RecipeHome.tsx
@@ -39,16 +39,16 @@ export function RecipeHome({ projects }: { projects: RecipeProject[] }) {
   return (
     <div className="flex-1 flex flex-col sm:p-4 space-y-12">
       <MarketplaceSection
-        header="No API key needed"
-        description="These APIs have zero auth setup! Use these in seconds..."
+        header="Use Right Away"
+        description="No API key required -> these APIs have zero auth setup! "
         projects={free}
       />
       <MarketplaceSection
         header="Popular APIs"
         description={
           <p>
-            Discover popular recipes built from popular APIs! We are open
-            sourced and need your help to build more recipes.{" "}
+            Discover recipes built from popular APIs! We are open source and
+            need your help to build more recipes.{" "}
             <a
               href={GITHUB_REPO}
               target="_blank"
@@ -133,7 +133,7 @@ export function RecipeHomeBox({
   return (
     <Link href={`/${project}`}>
       <div
-        className="border border-slate-700 rounded-md shadow-sm p-4 space-y-1 cursor-pointer h-full"
+        className="border border-slate-700 rounded-md shadow-sm p-4 space-y-1 cursor-pointer h-full bg-white dark:bg-slate-800"
         onClick={() => {
           postHog.capture(POST_HOG_CONSTANTS.PROJECT_LOAD, {
             project,
