6c23a10b0611d52cabe44c63e2ba334fe59dd483
Jeane Carlos <jeanefcarlos@gmail.com>
2023-08-18 13:11:22 -0700

    Faster tailwind conf

--- apps/desktop/tailwind.config.js
+++ apps/desktop/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "ui/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
