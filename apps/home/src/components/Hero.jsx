25a462da1091c541033e3f4c49f5683f39872829
Jeane Carlos <jeanefcarlos@gmail.com>
2023-08-21 11:55:25 -0700

    Upload demo page

--- apps/home/src/components/Hero.jsx
+++ apps/home/src/components/Hero.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 
 import { Container } from '@/components/Container'
@@ -5,8 +7,12 @@ import yc from '@/images/logos/yc.svg'
 import { Button } from './Button'
 import { HeroButtons } from '@/components/HeroButtons'
 import classNames from 'classnames'
+import DarkApp from '@/images/screenshots/DarkApp.png'
+import { useState } from 'react'
 
 export function Hero() {
+  const [show, setShow] = useState(false)
+
   return (
     <Container className="bg-black  text-center lg:pt-10">
       <h1 className="mx-auto max-w-4xl font-display text-4xl font-bold tracking-tight text-white sm:text-5xl marker:sm:text-7xl">
@@ -22,23 +28,65 @@ export function Hero() {
         <Image src={yc} alt="backed by yc" unoptimized className="w-40" />
       </div>
       <div
-        className={classNames(
-          'mx-auto mt-8 max-w-screen-xl',
-          // Revert this later when loom comesb ack,
-          'hidden'
-        )}
+        className={classNames('cursor-pointer')}
+        onClick={() => setShow(true)}
       >
-        <div className="aspect-w-16 aspect-h-9">
-          <iframe
-            title="Loom Video"
-            src="https://www.loom.com/embed/e5b8c04bca094dd8a5507925ab887002?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true"
-            allowFullScreen
-            className="h-full w-full"
-            style={{
-              width: '100%', // Adjust the width as needed
-              height: '100%', // Adjust the height as needed
-            }}
-          />
+        <div
+          style={{
+            position: 'relative',
+            paddingBottom: '64.5933014354067%',
+            height: 0,
+          }}
+        >
+          {show ? (
+            <iframe
+              src="https://www.loom.com/embed/a942d8382ea249dab99c8ba2792ae02c?sid=44ec3342-c13e-488d-a837-a5473d708e89"
+              frameborder="0"
+              webkitAllowFullscreen
+              mozAllowFullscreen
+              allowFullscreen
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+              }}
+            />
+          ) : (
+            <>
+              <Image
+                src={DarkApp}
+                alt="RecipeUI Dark Mode"
+                unoptimized
+                className="absolute left-0 right-0 top-0 max-h-[600px]  overflow-clip  object-cover object-top"
+              />
+              <div
+                className="absolute left-1/2 top-1/2 flex  items-center justify-center md:top-[30%]"
+                style={{
+                  transform: 'translate(-50%, -50%)',
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1}
+                  className="h-16 w-16 fill-slate-200 stroke-[#21b2a5] md:h-40 md:w-40"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.91 11.672a.375.375 0 010 .656l-5.603 3.113a.375.375 0 01-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+                  />
+                </svg>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </Container>
