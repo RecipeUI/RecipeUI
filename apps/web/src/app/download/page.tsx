c5a42b2cedba977947088ffaa81f62711cece68e
Jeane Carlos <jeanefcarlos@gmail.com>
2023-08-21 14:16:57 -0700

    Force version 0.2.0, unsure about stale github fetch

--- apps/web/src/app/download/page.tsx
+++ apps/web/src/app/download/page.tsx
@@ -5,20 +5,21 @@ import { DownloadContainer } from "@/app/download/DownloadContainer";
 export default async function DownloadPage() {
   let latestVersion: string = "0.2.0";
 
-  try {
-    const latestJson = await (
-      await fetch(
-        "https://github.com/RecipeUI/RecipeUI/releases/latest/download/latest.json"
-      )
-    ).json();
-    const version = latestJson.version;
+  // For some reason latest version is not propogating?
+  // try {
+  //   const latestJson = await (
+  //     await fetch(
+  //       "https://github.com/RecipeUI/RecipeUI/releases/latest/download/latest.json"
+  //     )
+  //   ).json();
+  //   const version = latestJson.version;
 
-    if (version) {
-      latestVersion = version;
-    }
-  } catch (e) {
-    console.error(e);
-  }
+  //   if (version) {
+  //     latestVersion = version;
+  //   }
+  // } catch (e) {
+  //   console.error(e);
+  // }
 
   return <DownloadContainer latestVersion={latestVersion} />;
 }
