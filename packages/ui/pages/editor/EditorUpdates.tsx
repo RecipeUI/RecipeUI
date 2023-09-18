8ab7fe237d822d6fa18f2fc70269c91139e9f8c7
Jeane Carlos <jeanefcarlos@gmail.com>
2023-09-18 07:17:07 -0400

    Change latest versioning

--- packages/ui/pages/editor/EditorUpdates.tsx
+++ packages/ui/pages/editor/EditorUpdates.tsx
@@ -3,11 +3,13 @@ import { useLocalStorage } from "usehooks-ts";
 import { LATEST_APP_VERSION } from "../../utils/constants/main";
 import { SparklesIcon } from "@heroicons/react/24/outline";
 import { isSemverLessThan } from "../../utils/main";
+import { useEffect, useMemo, useState } from "react";
 
 const VERSION_LOGS: {
   version: string;
   update: string;
   update_type?: "bug" | "feature";
+  ignore?: boolean;
 }[] = [
   {
     version: "0.8.3",
@@ -18,6 +20,7 @@ const VERSION_LOGS: {
     version: "0.8.2",
     update: "Bug fixes. Migrate to API storybook (more details soon).",
     update_type: "bug",
+    ignore: true,
   },
   {
     version: "0.8.0",
@@ -39,15 +42,41 @@ const VERSION_LOGS: {
     update: "Save requests as recipes that you can replay quickly.",
     update_type: "feature",
   },
-  {
-    version: "0.7.5",
-    update: "No one should see this.",
-  },
 ];
 
 export function EditorUpdates() {
   const { latestVersionDismiss, isLatest, setToLatestVersion } =
     useLatestVersionDetails();
+
+  const [showMore, setShowMore] = useState(false);
+
+  const versionLogs = useMemo(() => {
+    let versionLogs = VERSION_LOGS.filter((log) => {
+      if (showMore) {
+        return true;
+      }
+
+      if (log.ignore) {
+        return false;
+      }
+
+      if (
+        isSemverLessThan({
+          oldVer: log.version,
+          newVer: latestVersionDismiss,
+        })
+      ) {
+        return false;
+      }
+
+      return true;
+    });
+
+    if (!showMore) {
+      versionLogs = versionLogs.slice(0, 3);
+    }
+    return versionLogs;
+  }, [latestVersionDismiss, showMore]);
 
   if (isLatest) {
     return null;
@@ -61,16 +90,7 @@ export function EditorUpdates() {
 
       <div className="">
         <ul className="list-disc pl-[3%] space-y-2">
-          {VERSION_LOGS.map((log, i) => {
-            if (
-              isSemverLessThan({
-                oldVer: log.version,
-                newVer: latestVersionDismiss,
-              })
-            ) {
-              return null;
-            }
-
+          {versionLogs.map((log, i) => {
             return (
               <li key={i}>
                 <div className="flex items-start">
@@ -88,6 +108,16 @@ export function EditorUpdates() {
             );
           })}
         </ul>
+        {!showMore && (
+          <button
+            className="underline underline-offset-2 mt-2"
+            onClick={() => {
+              setShowMore(true);
+            }}
+          >
+            View past updates
+          </button>
+        )}
       </div>
       <button
         className="btn btn-outline btn-sm w-fit !mt-4 "
@@ -106,7 +136,7 @@ export function EditorUpdates() {
 export function useLatestVersionDetails() {
   const [latestVersionDismiss, setLatestVersionDismissed] = useLocalStorage(
     "latestVersion",
-    "0.7.6"
+    "0.8.0"
   );
 
   // useEffect(() => {
